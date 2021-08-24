82895f028ff91538f0a5f7bcf1fbe675f4ad6e1b
sis6789 <sis6789@gmail.com>
2021-08-25 08:50:51 +0900

    Serial Number generator for go routine

--- serial_number/serial_number.go
+++ serial_number/serial_number.go
@@ -1,19 +1,28 @@
 package serial_number
 
+import (
+	"fmt"
+	"log"
+)
+
 type SerialNumber struct {
 	int32Channel chan int32
+	active       bool
 }
 
 func New() *SerialNumber {
 	var sn SerialNumber
 	sn.int32Channel = make(chan int32, 5)
+	sn.active = true
 	go sn.generator()
 	return &sn
 }
 
 func (x *SerialNumber) generator() {
 	defer func() {
-		recover() // disable chan panic
+		recover()        // disable chan panic
+		x.active = false // set NA
+		fmt.Println("EOG")
 	}()
 	var num = int32(0)
 	for {
@@ -23,7 +32,12 @@ func (x *SerialNumber) generator() {
 }
 
 func (x *SerialNumber) Next() int32 {
-	return <-x.int32Channel
+	if x.active {
+		return <-x.int32Channel
+	} else {
+		log.Panicln("call after close.")
+		return -1
+	}
 }
 
 func (x *SerialNumber) Close() {
