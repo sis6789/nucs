304097ff11ebf534a46e4834fed30f0b6c7cee0d
sis6789 <sis6789@gmail.com>
2021-08-31 16:45:49 +0900

    fastq file record sequence를 식별자에 추가하여 R1과 R2가 연계되도록 함,

--- fmsc/fmsc.go
+++ fmsc/fmsc.go
@@ -7,15 +7,17 @@ type FMSC struct {
 	Molecular string `bson:"m"`
 	Side      string `bson:"s"`
 	Count     int    `bson:"c"`
+	Ordinal   int64  `bson:"o"`
 }
 
 // NewFMSC 새 FMSC 구조를 반환한다.
-func NewFMSC(file string, molecular string, side string, count int) FMSC {
+func NewFMSC(file string, molecular string, side string, count int, ordinal int64) FMSC {
 	return FMSC{
 		File:      file,
 		Molecular: molecular,
 		Side:      side,
 		Count:     count,
+		Ordinal:   ordinal,
 	}
 }
 
