07e765f27cd1812b8d491aba212d1714f37b1cf2
sis6789 <sis6789@users.noreply.github.com>
2021-09-23 16:29:06 +0900

    backup

--- keydb/bulk.go
+++ keydb/bulk.go
@@ -2,6 +2,7 @@ package keydb
 
 import (
 	"context"
+	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -12,13 +13,15 @@ type BulkBlock struct {
 	collectionName    string
 	collection        *mongo.Collection
 	accumulatedAction []mongo.WriteModel
+	modify            int
+	match             int
 }
 
-func NewBulk(collection string, applyLimit int) *BulkBlock {
+func NewBulk(collection string, interval int) *BulkBlock {
 	var x BulkBlock
 	x.collectionName = collection
 	x.collection = Col(x.collectionName)
-	x.limit = applyLimit
+	x.limit = interval
 	return &x
 }
 
@@ -41,16 +44,19 @@ func (x *BulkBlock) UpdateOne(model *mongo.UpdateOneModel) {
 }
 
 func (x *BulkBlock) Apply() {
+	var nonOrderedOpt = options.BulkWrite().SetOrdered(false)
 	if len(x.accumulatedAction) == 0 {
 		return
 	}
-	issueLen := len(x.accumulatedAction)
-	nonOrderedOpt := options.BulkWrite().SetOrdered(false)
 	var result *mongo.BulkWriteResult
 	if result, err = x.collection.BulkWrite(context.TODO(), x.accumulatedAction, nonOrderedOpt); err != nil {
 		log.Fatalln(err)
 	}
 	x.accumulatedAction = nil
-	log.Printf("%v bulk issue:%d match:%d modify:%d\n", x.collectionName, issueLen,
-		result.MatchedCount, result.ModifiedCount)
+	x.modify += int(result.ModifiedCount)
+	x.match += int(result.MatchedCount)
+}
+
+func (x *BulkBlock) String() string {
+	return fmt.Sprintf("match:%d modify:%d", x.match, x.modify)
 }
