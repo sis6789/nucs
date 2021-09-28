450ee239f413a28faddd42748ce39ad9a3b5c741
sis6789 <sis6789@users.noreply.github.com>
2021-09-29 08:42:19 +0900

    merge_values package

---
+++ merge_values/merge_values.go
@@ -0,0 +1,44 @@
+package merge_values
+
+import "log"
+
+//  merge return values as interface{}
+
+type GoSubResult struct {
+	wChan chan interface{}
+	V     []interface{}
+}
+
+func New() *GoSubResult {
+	var newResult GoSubResult
+	newResult.wChan = make(chan interface{})
+	// start receiver, repeat until closed
+	go func() {
+		for v := range newResult.wChan {
+			newResult.V = append(newResult.V, v)
+		}
+	}()
+	return &newResult
+}
+
+// Close - Terminate result merging go-routine
+func (x *GoSubResult) Close() {
+	defer func() {
+		v := recover()
+		if v != nil {
+			log.Println("close on closed channel")
+		}
+	}()
+	close(x.wChan)
+}
+
+// Send - send result
+func (x *GoSubResult) Send(v interface{}) {
+	defer func() {
+		v := recover()
+		if v != nil {
+			log.Println("send on closed channel")
+		}
+	}()
+	x.wChan <- v
+}
