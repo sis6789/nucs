da561e389ae5eb3a5b0f9fc45607da29a4b3e1b5
sis6789 <sis6789@users.noreply.github.com>
2021-09-28 09:50:28 +0900

    limitGoSub package

---
+++ limitGoSub/limitGoSub.go
@@ -0,0 +1,36 @@
+package limitGoSub
+
+type LimitGoSub struct {
+	waitChannel chan struct{}
+}
+
+func New(limit int) *LimitGoSub {
+	var wBlock LimitGoSub
+	wBlock.waitChannel = make(chan struct{}, limit)
+	return &wBlock
+}
+
+func (x *LimitGoSub) Close() {
+	defer func() {
+		_ = recover()
+	}()
+	close(x.waitChannel)
+}
+
+func (x *LimitGoSub) Wait() {
+	defer func() {
+		_ = recover()
+	}()
+	// wait until channel write success
+	x.waitChannel <- struct{}{}
+	return
+}
+
+func (x *LimitGoSub) Done() {
+	defer func() {
+		_ = recover()
+	}()
+	// make room at waitChannel
+	<-x.waitChannel
+	return
+}
