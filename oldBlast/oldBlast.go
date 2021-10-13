141713fca48148935da042f112afa4063ed50351
sis6789 <sis6789@users.noreply.github.com>
2021-10-13 11:21:50 +0900

    backup

--- oldBlast/oldBlast.go
+++ oldBlast/oldBlast.go
@@ -22,7 +22,6 @@ type OldBlast struct {
 	client      *keydb2.KeyDB
 	collection  *mongo.Collection
 	result      *mongo.SingleResult
-	bulk        *keydb2.BulkBlock
 	err         error
 }
 
@@ -33,7 +32,6 @@ func New(mongoAccess, dbName, colName string) *OldBlast {
 	x.colName = colName
 	x.client = keydb2.New(mongoAccess)
 	x.collection = x.client.Col(dbName, colName)
-	x.bulk = x.client.NewBulk(dbName, colName, 10000)
 	return &x
 }
 
@@ -50,41 +48,7 @@ func (x *OldBlast) Query(query string) bool {
 	return true
 }
 
-// Save - 블록에 데이터를 설정하고 save를 불러 저장한다. 오루가 없으면 true를 반환한다.
-func (x *OldBlast) Save() bool {
-	_, x.err = x.collection.InsertOne(context.TODO(), bson.D{
-		{"_id", x.B},
-		{"bt", x.BT},
-		{"c", x.C},
-		{"l", x.L},
-		{"qs", x.QS},
-		{"sf", x.SF},
-		{"ss", x.SS},
-		{"st", x.ST},
-	})
-	if x.err != nil {
-		return false
-	} else {
-		return true
-	}
-}
-
-// SaveBulk - 10000개 기준으로 저장한다. 최종 Close를 불러 저장해야 한다.
-func (x *OldBlast) SaveBulk() {
-	model1 := mongo.NewInsertOneModel().SetDocument(bson.D{
-		{"_id", x.B},
-		{"bt", x.BT},
-		{"c", x.C},
-		{"l", x.L},
-		{"qs", x.QS},
-		{"sf", x.SF},
-		{"ss", x.SS},
-		{"st", x.ST},
-	})
-	x.bulk.InsertOne(model1)
-}
-
 // Close - 모든 처리를 종료한다.
-func (x *OldBlast) Close() {
-	x.bulk.Close()
+func Close() {
+	//bulkOldBlast.Close()
 }
