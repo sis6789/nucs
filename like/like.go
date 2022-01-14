d31eadc9628aea2c1dca4ed56d69bac8921cc83f
sis6789 <sis6789@users.noreply.github.com>
2022-01-14 16:40:37 +0900

    molecular 돌출 부분 제거

--- like/like.go
+++ like/like.go
@@ -1,30 +1,44 @@
 package like
 
 import (
-	"regexp"
-
 	"github.com/biogo/biogo/align"
 	"github.com/biogo/biogo/alphabet"
+	"github.com/biogo/biogo/feat"
 	"github.com/biogo/biogo/seq/linear"
 )
 
-var smith = align.SW{
-	{0, -8, -8, -8, -8},
-	{-8, 4, -8, -8, -8},
-	{-8, -8, 4, -8, -8},
-	{-8, -8, -8, 4, -8},
-	{-8, -8, -8, -8, 4},
+var alphaWithBlank = alphabet.MustComplement(alphabet.NewComplementor(
+	"-acmgrsvtwyhkdbn *",
+	feat.DNA,
+	alphabet.MustPair(alphabet.NewPairing("acmgrsvtwyhkdbnxACMGRSVTWYHKDBNX- *", "tgkcysbawrdmhvnxTGKCYSBAWRDMHVNX- *")),
+	'-', 'n',
+	false,
+))
+
+var smith = align.SW{ // special for blank handling
+	{0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2},
 }
 
-//var smith = align.SW{
-//	{0, -1, -1, -1, -1},
-//	{-1, 2, -1, -1, -1},
-//	{-1, -1, 2, -1, -1},
-//	{-1, -1, -1, 2, -1},
-//	{-1, -1, -1, -1, 2},
-//}
-var splitExpStr = `\[(\d+),(\d+)\)/\[(\d+),(\d+)\)=(-?\d+)|\[(\d+),(\d+)\)/(-)=(-?\d+)|(-)/\[(\d+),(\d+)\)=(-?\d+)`
-var splitExp = regexp.MustCompile(splitExpStr)
+//var splitExpStr = `\[(\d+),(\d+)\)/\[(\d+),(\d+)\)=(-?\d+)|\[(\d+),(\d+)\)/(-)=(-?\d+)|(-)/\[(\d+),(\d+)\)=(-?\d+)`
+//var splitExp = regexp.MustCompile(splitExpStr)
 
 type scorer interface{ Score() int }
 type Match struct {
@@ -38,16 +52,17 @@ type Match struct {
 func Like(s1, s2 []byte) (Match, error) {
 	// use smith waterman
 	fsa := &linear.Seq{Seq: alphabet.BytesToLetters(s1)}
-	fsa.Alpha = alphabet.DNAgapped
-	fsb := &linear.Seq{Seq: alphabet.BytesToLetters(s2)}
-	fsb.Alpha = alphabet.DNAgapped
 
-	alns, err := smith.Align(fsa, fsb)
+	fsa.Alpha = alphaWithBlank
+	fsb := &linear.Seq{Seq: alphabet.BytesToLetters(s2)}
+	fsb.Alpha = alphaWithBlank // alphabet.DNAgapped
+
+	alignList, err := smith.Align(fsa, fsb)
 	if err != nil {
 		return Match{}, err
 	}
 	highestMatch := Match{Score: -1000}
-	for _, aln := range alns {
+	for _, aln := range alignList {
 		score := aln.(scorer).Score()
 		s1 := aln.Features()[0].Start()
 		e1 := aln.Features()[0].End()
